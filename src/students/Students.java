d16771c81c75bde3c6515ac8cb631997e635468f
srdjan93 <srdja0303@gmail.com>
2019-02-04 11:59:57 +0100

    Adds first commit

---
+++ src/students/Students.java
@@ -0,0 +1,71 @@
+
+package students;
+
+
+public class Students {
+    private String firstName;
+    private String lastName;
+    private int yearOfBirth;
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the yearOfBirth
+     */
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    /**
+     * @param yearOfBirth the yearOfBirth to set
+     */
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public Students() {
+        this.firstName = "Srdjan";
+        this.lastName = "Milosevic";
+        this.yearOfBirth = 1993;
+    }
+    
+    public Students(String firstName, String lastName, int yearOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.yearOfBirth = yearOfBirth;
+    }
+    
+    public void info() {
+        System.out.println("First name: " + this.firstName);
+        System.out.println("Last name: " + this.lastName);
+        System.out.println("Year of birth: " + this.yearOfBirth);
+    }
+    
+    
+}
